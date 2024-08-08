f61d5d07bd4dc2f2b98116832b3b9a724d2dadf1
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-08-08 18:35:47 +0600

    fix some code and complete about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import Span from './Span'
+import Blob from "/public/blob01.svg";
+import AboutIMage from "/public/About.png"
+import Image from 'next/image'
 const About = () => {
   return (
-    <div>About</div>
+    <section className='container mx-auto px-4 xl:px-0'>
+      <div className="flex items-center justify-center flex-col my-12">
+        <Span text={`about us`}/>
+        <h2 className="h2 text-center leading-snug font-extrabold">
+        best customer experience
+        </h2>
+      </div>
+      <div className="flex items-center justify-center">
+      <div className="flex items-center xl:flex-row md:flex-row  flex-col justify-center xl:justify-between xl:items-start xl:gap-24 gap-8 text-center">
+        {/* image */}
+        <div className="relative flex items-center justify-center">
+        <Image src={Blob} width={700} height={700} alt='blob' className='absolute top-0 left-0 rounded-[2rem]'/>
+          <Image src={AboutIMage} alt='car image' className='max-w-[800px] xl:max-w-full z-10'/>
+        </div>
+        {/* text */}
+        <div className="xl:text-left md:text-left text-left">
+          <h3 className='h2 xl:text-6xl text-4xl max-w-[24rem] xl:max-w-[32rem] font-extrabold leading-snug ' >
+          why should you <span className="text-orangePrimary">choose</span> us
+          </h3>
+          <p className="p text-balance text-sm px-2  xl:text-base xl:max-w-[27rem] capitalize">Choose a rental car location near your accommodation or major transport hubs like airports and train stations for convenience, ease of access, and a wider selection of vehicles.</p>
+        </div>
+      </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,22 +7,22 @@ import appStore from "/public/appstore.svg";
 const Hero = () => {
   return (
 
-    <section className="container mx-auto xl:my-28 my-14 px-4 xl:px-0 flex items-center justify-between  xl:flex-row flex-col text-center xl:text-left gap-16">
+    <section className="container mx-auto xl:my-28 my-14 px-4 xl:px-0 flex items-center justify-between  md:flex-row xl:flex-row flex-col text-center xl:text-left gap-16">
       {/* text */}
-      <div className="order-2 xl:order-1">
-        <h1 className="h1 capitalize xl:max-w-[40rem] text-6xl xl:text-8xl">
+      <div className="order-2 xl:order-1 md:order-1 md:text-left">
+        <h1 className="h1 md:text-5xl md:text-left capitalize xl:max-w-[40rem] text-6xl xl:text-8xl">
           <span className="text-orangePrimary">looking</span> for rent a car
         </h1>
-        <p className="p xl:max-w-[32rem] xl:font-light xl:text-lg capitalize px-8 xl:px-0">
+        <p className="p xl:max-w-[32rem] md:px-0 xl:font-light xl:text-lg capitalize px-8 xl:px-0">
           Our top priority is ensuring you have a seamless and enjoyable rental
           experience. Our friendly and knowledgeable staff are always ready to
           assist you .
         </p>
         <div className="flex flex-col my-3">
-          <p className="p text-xl xl:font-semibold capitalize my-0">
+          <p className="p md:text-base xl:text-xl xl:font-semibold capitalize my-0">
             download our latest app
           </p>
-          <div className="flex items-center justify-center xl:justify-start my-3 xl:gap-4 gap-3">
+          <div className="flex items-center justify-center md:justify-start xl:justify-start my-3 xl:gap-4 gap-3">
             <Image
               src={appStore}
               alt="playstore"
@@ -44,7 +44,7 @@ const Hero = () => {
             alt="blob"
             className="absolute top-0 right-0 rounded-[3rem]"
           />
-          <Image src={HeroImage} alt="hero image" className="z-10 xl:w-full" />
+          <Image src={HeroImage} alt="hero image" className="z-10 md:max-w-[500px] xl:max-w-full" />
         </div>
       </div>
     </section>
